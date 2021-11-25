662699b0282220f6ac433d64a647eb2003a921b5
AlexanderTymofeev <timofeev.mobile@gmail.com>
2021-11-25 20:13:46 +0200

    Добавляет выполненное ДЗ

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 'use strict';
 
 import './sass/main.scss';
-import axios from 'axios';
+
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { fetchImages } from './pixabay-api';
-// import { galleryCardsTemplate } from './tamplete';
 import galleryCardsTemplate from './templates/gallery-card.hbs';
 
 const searchFormEl = document.querySelector('.search-form');
@@ -20,7 +19,7 @@ const showLoadMoreBtn = () => {
   loadMoreBtn.classList.remove('is-hidden');
 };
 
-const onFormSubmit = event => {
+const onFormSubmit = async event => {
   event.preventDefault();
 
   localStorage.setItem('page', '1');
@@ -29,45 +28,46 @@ const onFormSubmit = event => {
   localStorage.setItem('query', query);
   clearGallery();
 
-  fetchImages(query)
-    .then(data => {
-      showLoadMoreBtn();
-      if (data.hits.length === 0) {
-        Notify.failure(`Sorry, there are no images matching your search query. Please try again.`);
-        loadMoreBtn.classList.add('is-hidden');
-      }
-      galleryEl.insertAdjacentHTML('beforeend', galleryCardsTemplate(data.hits));
-      // console.log(data);
-    })
-    .catch(err => {
-      console.log(err);
-    });
+  try {
+    const { data } = await fetchImages(query);
+
+    showLoadMoreBtn();
+    if (data.hits.length === 0) {
+      Notify.failure(`Sorry, there are no images matching your search query. Please try again.`);
+      loadMoreBtn.classList.add('is-hidden');
+    }
+
+    galleryEl.insertAdjacentHTML('beforeend', galleryCardsTemplate(data.hits));
+    let pageIterattor = Number(localStorage.getItem('page')) + 1;
+    // console.log(pageIterattor);
+    localStorage.setItem('page', JSON.stringify(pageIterattor));
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-const onLoadMoreBtnClick = () => {
+const onLoadMoreBtnClick = async () => {
   const query = localStorage.getItem('query');
 
-  fetchImages(query)
-    .then(data => {
-      galleryEl.insertAdjacentHTML('beforeend', galleryCardsTemplate(data.hits));
-      const pageIterattor = Number(localStorage.getItem('page')) + 1;
-      console.log(pageIterattor);
-      localStorage.setItem('page', JSON.stringify(pageIterattor));
-    })
-    .catch(err => {
-      console.log(err);
-    });
+  try {
+    const { data } = await fetchImages(query);
+
+    galleryEl.insertAdjacentHTML('beforeend', galleryCardsTemplate(data.hits));
+
+    let pageIterattor = Number(localStorage.getItem('page'));
+    // console.log(pageIterattor);
+
+    if (data.totalHits <= pageIterattor * 40) {
+      loadMoreBtn.classList.add('is-hidden');
+      Notify.failure(`We're sorry, but you've reached the end of search results.`);
+    }
+    pageIterattor += 1;
+    localStorage.setItem('page', JSON.stringify(pageIterattor));
+  } catch (err) {
+    console.log(err);
+  }
 };
-
-// function onEnd() {
-//   if (page >= 13) {
-//     loadMoreBtn.classList.add('is-hidden');
-//     Notify.failure(`!`);
-//   }
-//   return;
-// }
-
-// onEnd();
 
 searchFormEl.addEventListener('submit', onFormSubmit);
 loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
--- src/pixabay-api.js
+++ src/pixabay-api.js
@@ -3,17 +3,13 @@
 const API_KEY = '24502852-6a795cd657d1b023a90d357f4';
 const BASE_URL = 'https://pixabay.com/api/';
 
-export const fetchImages = query => {
+import axios from 'axios';
+
+export const fetchImages = async query => {
   let page = Number(localStorage.getItem('page'));
-  page += 1;
 
-  return fetch(
+  const result = await axios.get(
     `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`,
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-
-    return response.json();
-  });
+  );
+  return result;
 };
